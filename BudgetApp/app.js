d8b34b16401c18236470b3fc3a8dddb45323430a
Lukas Komprs <komprs.l@gmail.com>
2020-04-10 08:47:15 +0200

    Setting up the First Event Listeners

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -1,67 +1,41 @@
-//!!! All of the logic encapsulation works coz of closures
+//Budget Controller
+var budgetController = (function() { 
 
-//Due to closures an inner func has always access to its parent varia, func even after outer func has returned
-
-
-// IIFE is simply an anonymous func wrapped in parenthesis
-//and it return an object containing all of the funcs that we want to be public, so the funcs we want to give outside scope access to
-
-//So the IIFE returns immedia. and so it's effectively gone but the public test func that we returned here will always has access to the x variable and the add func
-/*
-var budgetController = (function() { //anonymous func that is immed invoked and in this case it return publicTest mehtod
-    
-    var x = 23; //x and add are private coz they are in the closure and therefore only the publicTest meth can access them
-    
-    var add = function(a) {
-        return x + a;
-    }
-    
-    return {
-        publicTest: function(b) {
-            console.log(add(b));
-        }
-    }
-}) (); 
-
-Moduel can accept arguments coz they are just function expressions
-
-
-*/
-
-var budgetController = (function() { //this func returns publicTest obj
-    
-    var x = 23; //x and add are private coz they are in the closure and therefore only the publicTest meth can access them
-    
-    var add = function(a) {
-        return x + a;
-    }
-    
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
 }) ();
 
+
+//UI Controller
 var UIController = (function () {
     
 }) ();
 
 
-// we will pass other 2 modules as arguments to the controller, so this one know about other 2, and connect them, or we could call them directly coz they are in outer scope but not ideal if we change name of our modules
+//Global App controller
 var controller = (function(budgetCtrl, UICtrl){
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function () {
+    //1. Get the field input data
     
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    //2. Add the item to the budget  controller
+    
+    // 3. Add the item to the UI
+    
+    // 4. Calculate the budget 
+    
+    // 5. Display the budget on the UI
+        console.log('work');
     }
-}) (budgetController, UIController);// we pass other 2 modules inside
-
-
-
-
-
-
+    
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem); 
+        
+    
+    
+    //event gets automatically passes by browser to our event listener
+    document.addEventListener('keypress', function(event) {
+        //which for older browsers
+        if(event.keyCode === 13 || event.which === 13){
+           ctrlAddItem();
+        }
+    });
+    
+}) (budgetController, UIController);
 
