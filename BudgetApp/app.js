48d05c6b5a3d6e9383cea910dd47b31623920728
Lukas Komprs <komprs.l@gmail.com>
2020-04-11 10:01:19 +0200

    Reading Input Data

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -1,20 +1,42 @@
 //Budget Controller
-var budgetController = (function() { 
-
-}) ();
+var budgetController = (function(contrl) { 
+contrl.publicTest;
+}) (controller);
 
 
 //UI Controller
 var UIController = (function () {
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn' 
+    };
     
+    return {
+        getInput: function() { // will be either inc or exp the value
+            return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }
 }) ();
 
 
-//Global App controller
+//Global App controller, we contral other modules and call mehtods
 var controller = (function(budgetCtrl, UICtrl){
-    var ctrlAddItem = function () {
-    //1. Get the field input data
-    
+    var DOM = UICtrl.getDOMstrings(); 
+    var input;
+    var ctrlAddItem = function(){         
+        //1. Get the field input data
+        input = UICtrl.getInput()
+        console.log(input);
+        
     //2. Add the item to the budget  controller
     
     // 3. Add the item to the UI
@@ -22,13 +44,10 @@ var controller = (function(budgetCtrl, UICtrl){
     // 4. Calculate the budget 
     
     // 5. Display the budget on the UI
-        console.log('work');
+     
     }
-    
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem); 
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem); 
         
-    
-    
     //event gets automatically passes by browser to our event listener
     document.addEventListener('keypress', function(event) {
         //which for older browsers
@@ -36,6 +55,11 @@ var controller = (function(budgetCtrl, UICtrl){
            ctrlAddItem();
         }
     });
-    
+
+       return{ 
+        publicTest: function (){
+            return input;
+        }
+    }
 }) (budgetController, UIController);
 
