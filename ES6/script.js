aae0504f50e6f3f83a509de89e879cabf58430d7
Lukas Komprs <komprs.l@gmail.com>
2020-04-15 13:58:00 +0200

    Spread Operator

--- ES6/script.js
+++ ES6/script.js
@@ -291,6 +291,7 @@ for(var i = 0; i < boxesArr.length; i++){
 */
 
 //ES6
+/*
 for(const cur of boxesArr){
     if(cur.className.includes('blue')){
         continue;
@@ -316,6 +317,40 @@ ages.findIndex((cur => cur >= 18));
 
 //to find the value that is greater than 18
 console.log(ages.find(cur => cur >=18));
+
+*/
+
+/////////////////////////////////
+// Lecture: Spread operator
+
+//ES5
+
+function addFourAges(a,b,c,d){
+    return a + b + c + d;
+}
+var sum = addFourAges(18,30,20,21);
+var ages = [18,30,12,21];
+
+var sum2 = addFourAges.apply(null, ages); //use the ages array as arguments
+
+console.log(sum2);
+
+/// ES6
+
+const max3 = addFourAges(..ages); //new spread operator
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+const bigFamily = [...familySmith, 'Lily', ...familyMiller];
+console.log(bigFamily);
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ..boxes];
+
+//nodelist to array
+Array.from(all).forEach( cur => cur.style.color = 'purple');
+
 
 
 
