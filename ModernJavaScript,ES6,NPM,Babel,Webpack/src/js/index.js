41ea50a580743df773acec2c72106594a5d03205
Lukas Komprs <komprs.l@gmail.com>
2020-04-27 00:05:36 +0200

    First Api Call Using Axios

--- ModernJavaScript,ES6,NPM,Babel,Webpack/src/js/index.js
+++ ModernJavaScript,ES6,NPM,Babel,Webpack/src/js/index.js
@@ -1,5 +1,15 @@
-import str from './models/Search';
+import axios from 'axios';
+async function getResults(query){
+    try{ 
+        //it does ajax call and returns a promise
+         const res = await  axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`); //automatically returns json
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch(error){
+        alert(error);
+    }
+}
 
-//import {add as a,multiply as b} from './views/searchView'; //as to in order to change name
-import * as searchView from './views/searchView'; //to import everyting and save it as an object
-console.log(`Using imported func! ${searchView.multiply(3,5)}`);
+//http://forkify-api.herokuapp.com
+
+getResults('pizza');
