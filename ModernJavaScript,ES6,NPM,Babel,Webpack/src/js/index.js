29dbf8312c1008da6d3bb277259ada876accc093
Lukas Komprs <komprs.l@gmail.com>
2020-04-26 23:33:52 +0200

    How to export/import

--- ModernJavaScript,ES6,NPM,Babel,Webpack/src/js/index.js
+++ ModernJavaScript,ES6,NPM,Babel,Webpack/src/js/index.js
@@ -1,1 +1,5 @@
-// Global app controller
+import str from './models/Search';
+
+//import {add as a,multiply as b} from './views/searchView'; //as to in order to change name
+import * as searchView from './views/searchView'; //to import everyting and save it as an object
+console.log(`Using imported func! ${searchView.multiply(3,5)}`);
