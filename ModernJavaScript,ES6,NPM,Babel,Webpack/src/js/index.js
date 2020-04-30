9e5f358e84f5a10ef04ddaa019a61ae5c0c76695
Lukas Komprs <komprs.l@gmail.com>
2020-04-30 11:20:23 +0200

    Unify units from Title

--- ModernJavaScript,ES6,NPM,Babel,Webpack/src/js/index.js
+++ ModernJavaScript,ES6,NPM,Babel,Webpack/src/js/index.js
@@ -18,9 +18,9 @@ import Recipe from './models/Recipe';
 const state = {};
 
 const controlSearch = async () => {
-    // 1) Get query from view
-    
-    const query = searchView.getInput();
+    // 1) Get query from view  
+    //const query = searchView.getInput();
+    const query = 'pizza';
 
     if(query){
         // 2) New Search object and add to state
@@ -31,17 +31,21 @@ const controlSearch = async () => {
         searchView.clearResults(); //clear old results
         renderLoader(elements.searchRes); //put the spinning arrow while we are wainting
 
-        //4) Search for recipes
-            //we wait until getResults return and only then it continous to step 5
-        await state.search.getResults(); //returns a promise evry async func
+        try{ 
+            //4) Search for recipes
+                //we wait until getResults return and only then it continous to step 5
+            await state.search.getResults(); //returns a promise evry async func
 
-        //5) Render results on UI
-        removeArrow();  
-        searchView.renderResults(state.search.result);
-          
-       
+            //5) Render results on UI
+            removeArrow(); 
+            searchView.renderResults(state.search.result);
+        } catch(error){
+            alert(`Something went wrong`);
+            removeArrow(); 
+        }
     }
 }
+
 
 elements.searchForm.addEventListener('submit', e =>{
    /** 
@@ -52,13 +56,26 @@ elements.searchForm.addEventListener('submit', e =>{
     controlSearch();
 });
 
+
+//for TESTING
+window.addEventListener('load', e =>{
+    /** 
+     * whenever we submit a form we prevent reload the page
+     * and call our callback func
+    */
+     e.preventDefault();
+     controlSearch();
+ });
+
+
+
  
 /**
  * Event Delegation
  * Event delegation allows us to attach a single event listener, to a parent element, that will fire for all descendants matching a selector, whether those descendants exist now or are added in the future
  * We attach the event listeners to an element that is already there, and then we try to figure out where  the click happened
 */
-elements.searchResPages.addEventListener('click', e=>{
+elements.searchResPages.addEventListener('click', e=>{//button functionality
     const btn = e.target.closest('.btn-inline'); //we are only intersted in the ones with the class of button inline
     if (btn){
         const goToPage = parseInt(btn.dataset.goto, 10);
@@ -71,10 +88,41 @@ elements.searchResPages.addEventListener('click', e=>{
 
 
 /**
- * RECiPe CONTROLLER
+ * RECIPE CONTROLLER //////////////////////////////////////////
  */
 
- const r = new Recipe(47025);
-r.getRecipe();
-console.log(r);
+ const controlRecipe = async () => {
+     //window.location whole search bar, hash is a string and we can call all string methods on it
+    //we need our # symbol in url to use hash
+     const id = window.location.hash.replace('#','');
+    console.log(id);
 
+    if(id){
+        //Prepare UI for changes
+
+        //Create a new recipe object based on the Recipe model and save it in our state, same like we did wit the search
+        state.recipe = new Recipe(id);
+        //TESTINg now we have acces it is in the global object
+        window.r = state.recipe;
+        try{ 
+                //Get Recipe data it return prome so we wait (await) before we continue
+                await state.recipe.getRecipe();// getRecipe() method, we need to await for the results returned from the API. Only then we can assign the data returned from the API to appropriate properties.In the controlRecipe() function, we can't go further without the values assigned in the getRecipe() function. That's why we also need to await here.
+
+                state.recipe.parseIngrediens();
+                //Calcutale servings and time
+                state.recipe.calcTime();
+                state.recipe.calcServings();
+
+                //Render the recipe
+                console.log(state.recipe);
+                // no state holds 2 objects: {search: Search, recipe: Recipe}
+            // console.log(state);
+        } catch(error){
+            alert(error);
+        }
+    }
+ };
+
+ //window.addEventListener('hashchange', controlRecipe);
+ //windows.addEventListener('load',controlRecipe);, The load event is fired when the whole page has loaded
+['hashchange','load'].forEach(event => window.addEventListener(event, controlRecipe));
--- ModernJavaScript,ES6,NPM,Babel,Webpack/src/js/models/Recipe.js
+++ ModernJavaScript,ES6,NPM,Babel,Webpack/src/js/models/Recipe.js
@@ -9,7 +9,7 @@ export default class Recipe {
        try{
             const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
             this.title = res.data.recipe.title;
-            this.auther = res.data.recipe.publisher;
+            this.author = res.data.recipe.publisher_url;
             this.img = res.data.recipe.imgage;
             this.url = res.data.recipe.source_url;
             this.ingredients = res.data.recipe.ingredients;
@@ -29,4 +29,33 @@ export default class Recipe {
    calcServings() {
        this.servings = 4;
    }
+
+   parseIngrediens() {
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces','ounce', 'teaspoons','teaspoon','cups', 'pounds'];
+        const unitsShort = ['tbsp', 'tbsp','oz','oz', 'tsp','tsp','cup', 'pound'];
+       
+
+        /**
+         * First round we take ingredient from index 0 ('One teaspoon of sugar') and apply modification on this string after our inner loops ends we move to other string
+         * In each iteration of map we return our modified string and save it to newIngredients array
+         */
+        const newIngredients = this.ingredients.map(el => {
+            //console.log(`${el} hola`);
+                //Uniform units
+                let ingredient = el.toLowerCase();
+                    //loop over the long unit and replace it by short unit if we find a match in our current ingredient string  
+                    unitsLong.forEach((unit, i) =>{
+                        ingredient = ingredient.replace(unit,unitsShort[i]);
+                    });
+
+                // 2) Remove parentheses
+                ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+
+                 // 3) Parse ingredients into count, unit and ingredient
+
+            return ingredient;
+            });
+
+        this.ingredients = newIngredients;
+    }
 }
